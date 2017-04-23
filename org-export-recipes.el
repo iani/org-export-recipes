70f451747a5e018deb491ed668baf125f716d1c6
Ioannis Zannos <iani@chamade.avarts.ionio.gr>
2017-04-23 11:09:06 +0300

    greek recipe added

--- org-export-recipes.el
+++ org-export-recipes.el
@@ -16,10 +16,11 @@
 (defun org-export-insert-recipe ()
   "Select and insert file from this folder to org-moe file."
   (interactive)
-  (message org-export-recipe-folder)
   (let ((recipe
          (helm-read-file-name "select-recipe:" :initial-input org-export-recipe-folder)))
-    (insert-file-contents recipe)))
+    (save-excursion
+      (goto-char 0)
+     (insert-file-contents recipe))))
 
 (org-defkey org-mode-map (kbd "H-c i") 'org-export-insert-recipe)
 
